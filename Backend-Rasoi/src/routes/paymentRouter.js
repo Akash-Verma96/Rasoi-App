0d46df09461c9cd5902c370423a8ef97f043960b
Akash-Verma96 <av3764909@gmail.com>
2026-05-31 22:56:17 +0530

    bug fixed

--- Backend-Rasoi/src/routes/paymentRouter.js
+++ Backend-Rasoi/src/routes/paymentRouter.js
@@ -2,7 +2,7 @@ import express from "express";
 import userAuth from "../middlewares/auth.js";
 import razorpayInstance from "../utils/razorpay.js";
 import Payment from "../models/payment.js";
-import validateWebhookSignature from 'razorpay/dist/utils/razorpay-utils.js';
+import validateWebhookSignature from "razorpay/dist/utils/razorpay-utils.js";
 
 const paymentRouter = express.Router();
 
@@ -10,8 +10,6 @@ paymentRouter.post("/payment/create", userAuth, async (req, res) => {
   try {
     const { name, phone } = req.body;
 
-
-    
     const order = await razorpayInstance.orders.create({
       amount: 5000,
       currency: "INR",
@@ -23,8 +21,7 @@ paymentRouter.post("/payment/create", userAuth, async (req, res) => {
       },
     });
 
-    
-    // Save OrderID to database 
+    // Save OrderID to database
     const payment = new Payment({
       userId: req.user._id,
       orderId: order.id,
@@ -34,7 +31,6 @@ paymentRouter.post("/payment/create", userAuth, async (req, res) => {
       receipt: order.receipt,
       notes: order.notes,
     });
-
 
     // save payment here
 
@@ -48,44 +44,42 @@ paymentRouter.post("/payment/create", userAuth, async (req, res) => {
 
 paymentRouter.post("/payment/webhook", async (req, res) => {
   try {
-      const webhookSignature = req.headers('x-razorpay-signature');
-      console.log("Webhook Signature:", webhookSignature);
+    const webhookSignature = req.get("x-razorpay-signature");
+    console.log("Webhook Signature:", webhookSignature);
 
-      const isValidSignature = validateWebhookSignature(JSON.stringify(req.body), webhookSignature, process.env.RAZORPAY_WEBHOOK_SECRET);
+    const isValidSignature = validateWebhookSignature(
+      JSON.stringify(req.body),
+      webhookSignature,
+      process.env.RAZORPAY_WEBHOOK_SECRET,
+    );
 
-      if(!isValidSignature) {
-          return res.status(400).send("Invalid signature");
-      }
-      console.log("Valid webhook Signature received");
+    if (!isValidSignature) {
+      return res.status(400).send("Invalid signature");
+    }
+    console.log("Valid webhook Signature received");
 
-      // update database with payment status
+    // update database with payment status
 
-      const paymentDetail = req.body.payload.payment.entity;
+    const paymentDetails = req.body.payload.payment.entity;
 
-      const payment = await Payment.findOne({ orderId: paymentDetail.order_id });
-      if(payment) {
-        payment.status = paymentDetail.status;
-        await payment.save();
-      }
+    const payment = await Payment.findOne({ orderId: paymentDetails.order_id });
+    payment.status = "captured";
+    await payment.save();
 
-      // update the payment status in frontend
+    // update the payment status in frontend
 
+    // Process the successful payment event
+    if (req.body.event === "payment.captured") {
+    }
+    // Process the failed payment event
+    if (req.body.event === "payment.failed") {
+    }
 
-
-      // Process the successful payment event
-      if(req.body.event === "payment.captured") {
-
-      }
-      // Process the failed payment event
-      if(req.body.event === "payment.failed") {
-
-      }
-
-      // send a response to acknowledge receipt of the webhook
-      res.status(200).send("Webhook received");
+    // send a response to acknowledge receipt of the webhook
+    return res.status(200).json({ msg: "Webhook received successfully" });
   } catch (error) {
     res.status(500).send(error.message);
   }
-})
+});
 
 export default paymentRouter;
